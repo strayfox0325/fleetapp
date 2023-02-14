09771dbcfc409f9482734edb1a35b8536a182293
Isidora Lazic <ilazic@UOS.local>
2023-02-14 13:33:09 +0100

    initial commit

---
+++ src/test/java/com/isidora/fleetapp/FleetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isidora.fleetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
