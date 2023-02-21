c5fe15aa6b3bd944f7e2905751673fc52a3063dd
Isidora Lazic <ilazic@UOS.local>
2023-02-21 12:11:53 +0100

    Created pages and implemented layout page decorating

--- src/main/java/com/isidora/fleetapp/ApplicationController.java
+++ src/main/java/com/isidora/fleetapp/ApplicationController.java
@@ -10,4 +10,33 @@ public class ApplicationController {
     public String showIndex() {
         return "index";
     }
+
+    @GetMapping("/hr")
+    public String showHR() {
+        return "/hr/index";
+    }
+
+    @GetMapping("/fleet")
+    public String showFleet() {
+        return "/fleet/index";
+    }
+
+    @GetMapping("/accounts")
+    public String showAccounts() {
+        return "/accounts/index";
+    }
+
+    @GetMapping("/helpdesk")
+    public String showHelpdesk() {
+        return "/helpdesk/index";
+    }
+
+    @GetMapping("/parameters")
+    public String showParameters() {
+        return "/parameters/index";
+    }
+    @GetMapping("/payroll")
+    public String showPayroll() {
+        return "/payroll/index";
+    }
 }
