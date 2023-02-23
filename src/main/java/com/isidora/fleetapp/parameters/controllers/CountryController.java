de712c3ec4ee6125de38c413877514832400153f
Isidora Lazic <ilazic@UOS.local>
2023-02-23 15:38:40 +0100

    Country list table

--- src/main/java/com/isidora/fleetapp/parameters/controllers/CountryController.java
+++ src/main/java/com/isidora/fleetapp/parameters/controllers/CountryController.java
@@ -4,6 +4,7 @@ import com.isidora.fleetapp.parameters.models.Country;
 import com.isidora.fleetapp.parameters.services.CountryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -16,8 +17,9 @@ public class CountryController {
     private CountryService countryService;
 
     @GetMapping("/countries")
-    @ResponseBody // return JSON
-    public List<Country> getAll() {
-        return  countryService.getAll();
+    public String getAll(Model model) {
+        List<Country> countries = countryService.getAll();
+        model.addAttribute("countries", countries);
+        return "parameters/countryList";
     }
 }
